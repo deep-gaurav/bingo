4af5a9c20a28e1931c9fbaeb906a4b62544cea34
Deep Gaurav <deepgauravraj@gmail.com>
2023-01-25 23:32:48 +0530

    remove flags

--- build_vercel.sh
+++ build_vercel.sh
@@ -4,4 +4,4 @@ git clone -b stable https://github.com/flutter/flutter.git
 ./flutter/bin/flutter doctor
 ./flutter/bin/flutter clean
 ./flutter/bin/flutter pub get
-./flutter/bin/flutter build web --dart-define=BACKEND_URL=$BACKEND_URL --web-renderer canvaskit --release --verbose --dart-flags=verbose_gc
+./flutter/bin/flutter build web --dart-define=BACKEND_URL=$BACKEND_URL --web-renderer canvaskit --release --verbose
