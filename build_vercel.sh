86992c139e27e081817e27265b3bb756c36ec928
Deep Gaurav <deepgauravraj@gmail.com>
2023-01-25 23:20:34 +0530

    fixes

--- build_vercel.sh
+++ build_vercel.sh
@@ -1,3 +1,7 @@
 git clone -b stable https://github.com/flutter/flutter.git
+./flutter/bin/flutter precache
+
 ./flutter/bin/flutter doctor
-./flutter/bin/flutter build web --dart-define=BACKEND_URL=$BACKEND_URL --web-renderer canvaskit --release
+./flutter/bin/flutter clean
+
+./flutter/bin/flutter build web --dart-define=BACKEND_URL=$BACKEND_URL --web-renderer canvaskit --release --verbose
